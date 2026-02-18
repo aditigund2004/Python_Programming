79baeac1af478b575d15ad5ce24c25a95682ef93
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-02-18 21:28:01 +0530

    Add files via upload
    exception handling is a mechanism to handle runtime errors using try, except, else, and finally blocks.

---
+++ FileHandlingEx.py
@@ -0,0 +1,245 @@
+'''
+
+Basic terminology of programming language
+
+1.compiler - low level convert
+2.High leve - .class create (source code)
+3.low level - machine code (.pyc) python compile file
+4.interpeter - line by line code execute
+5.algorithm - step by step to execute the program
+
+'''
+
+
+'''
+python have interpreter language and have
+compiler and interpret both the python have
+
+
+'''
+
+
+'''
+
+.java - run on PVM - python virtual machine
+'''
+
+
+
+'''
+cthon
+json  - java python
+
+JSON (JavaScript Object Notation) is a lightweight, text-based, 
+language-independent data-interchange format.
+'''
+
+
+'''
+what is python ->
+    1.high level
+    2.dynamic type language - 
+    3.interpreter
+    4.easy to read and write
+    5.oops support
+    6.free and open souce
+    7.general purpose  - (all type) it is not designed for one specific area, but rather is versatile 
+    enough to be used across a wide range of application domains
+    8.support multiple paradigam
+
+'''
+
+'''
+python feature -?
+
+    1.platform independent
+    2.use for web development
+    3.auto memeory management
+    4.standrd library
+    5.support multiple programming
+
+'''
+
+
+'''
+dynamic type -> js, python, R  (no need to declare data type)
+static type -> java
+
+
+'''
+
+
+'''
+.py -> create file
+.pyc ->   (generate compiler and uderstand machine low level)A .pyc file contains the compiled bytecode of a Python source file, which
+the Python interpreter generates to speed up the loading and startup time of Python 
+programs. These files are not human-readable or meant for manual editing
+
+
+'''
+
+
+'''
+platfrom independent -> write once run enywhere (.pyc) 
+Platform independence means software can run on any platform (operating system and architecture)
+without needing modification or recompilation. 
+
+cross platform independent -> Cross-platform means the software is 
+available on multiple platforms, but may require a specific build or interpreter for each one.
+
+
+
+platform in -> code portability
+
+cross platform -> application portability
+UI ->
+tkinter
+kivy
+
+'''
+
+
+'''
+windows - gui
+linux - command 
+
+'''
+
+'''
+What is variable -> store dataype
+
+container to store value
+onject is variable
+
+
+identifier - it is protected variable
+
+
+identity of the 
+and it must not be keyword
+
+
+
+_ (single undercore) - > protected variable
+__ (double underscore) - > privated variable
+
+'''
+
+'''
+pastel case ->
+camel case -> fiest not another upport
+sneck case - < using undercore
+
+naming convention -> A-Z, a-z, allowd
+numeric allowd but last of the variable ex: num1
+
+
+Camel case -> (often styled camelCase) is a capitalization convention 
+used in programming and text to join words without spaces, where each word 
+begins with a capital letter except for the first one (e.g., myVariableName)
+
+'''
+
+num1 = num2 = 30
+#  muliple varible assign single value
+
+
+num1, num2 = 20 ,30
+
+# swap
+num1 , num2 = num2, num1
+
+# fibonacic
+# temp = 0
+# temp = num1
+# num1 = num2
+# num2 = num1 + temp
+
+# print(temp)
+
+# alternate way to write code of fibonacies
+num1, num2 = num2, num1 + num2
+
+
+
+'''
+datatype -> 
+fundamental -> int, float. string, store immutable datatype with single value
+
+memoery assign -> memroy assign to value
+num1 = 10
+num2 = 10
+num3 = 10
+
+assign one merory for tempory for all variable
+
+collective -> a dataype contain multiple (collection of more than on element)
+memory assign
+l1 [10,20,30]
+
+immutable not changeble 
+
+
+mutable changeble - > CRUD operation perform
+
+
+del apply on mutable and immutable
+
+'''
+
+'''
+Classification of DT ->
+ 
+ 
+
+sequnce - > 
+
+map -> dicyonaru
+
+unorder - > set 
+'''
+
+'''
+Built in data structure in py ->
+list , set, tuple, 
+
+
+string  - colection character and it is immutable and written as '', ""
+, triple coute
+'''
+
+str1 = "welcome'student'"
+
+str2 = 'welcom "student"'
+
+str1[5:1:-1]
+
+print(str1)
+
+str1[-5:-1]
+print(str1)
+'''
+index -> fetach one elemnet
+
+positive and nagative
+
+
+slicing -> 
+defualt -1
+start , end ,step
+'''
+
+'''
+lower -> method
+
+
+casefold - > method cover to lower for case sensitive
+
+
+find -> pass value 
+return -1 it does not return error
+return firxt occurence
+
+index - >(pass value) index number
+return error
+'''
