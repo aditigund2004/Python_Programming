ac76e28ff3ee2a3f7d2137eac8e64f1f73d07762
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-02-21 22:47:34 +0530

    Add files via upload
    python module is a single file containing Python code (functions, classes, and variables) that can be reused in other programs.

---
+++ module1.py
@@ -0,0 +1,10 @@
+'''
+A Python module is a single file containing Python code (functions, classes, and variables) that can be reused in other programs. The file name is the module name with a .py extension. Modules help organize code, improve maintainability, and prevent naming conflicts by using separate namespaces
+'''
+
+
+
+
+
+product = 'zara'
+price = 50000
---
+++ module2.py
@@ -0,0 +1,17 @@
+dis = 10
+import module1
+sp = module1.price-module1.price * dis/100
+print('selling price',sp)
+
+from module1 import product,price
+# from module1 import *
+print('product name',product)
+print('price ',price)
+
+
+'''
+[] list
+'''
+from keyword import kwlist
+print(kwlist)
+print(len(kwlist))
