9691145ad77b8d41e989a7b57b6f0830501cd345
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-03-22 21:44:50 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,32 @@
+#String formatting
+
+print("hello welcome to the nexanova protech orgnisation shivajinagar pune")
+
+#def keyward used to define function
+# * = contain or have many values
+
+#parameter - pass variable
+#argument pass value
+
+# id() - object passing and only pass only one parameter seperating by comma , 
+# ex print(name, age, no)
+# id() - memory location or addresss
+# type() - return data tyoe of object
+
+
+n1 = 1
+n2 = 3
+n3 = 4
+print(n1,n2,n3)
+print(n1,n2,n3, sep='-')
+print(n1,n2,n3, sep='')
+print(n1,n2,n3, end=' ')
+
+
+print(id(n1))
+print(id(n2))
+
+print(type(n1))
+
+
+        
