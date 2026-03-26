364e4b4f2508a28253ba5a1321338c3cffcd3d48
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-03-26 21:33:21 +0530

    Add files via upload

---
+++ iterativeEx.py
@@ -0,0 +1,133 @@
+'''
+iterative statement
+looping stat ->
+
+1. for loop -> know the number of execution
+
+
+iterable - execute multiple times using for loop 
+
+
+2. while loop
+
+'''
+
+# task
+'''
+5 student marks
+'''
+
+
+# fruits = ["apple", "banana", "mango"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+
+
+# student = {'ram':80,'sham':90,'jay':89}
+
+# for name, marks in student.items():
+#     print(name, marks)
+
+
+
+# dict = {}
+# for i in range(5):
+#     name = input("Enter name :")
+#     mark = eval(input("Enter marks"))
+#     result[name]=mark
+# print(result)
+    
+
+
+    
+# nested loop
+
+
+# result = {}
+# ns = int(input("Enter number of student"))
+# nt = int(input("enter number of test"))
+# for i in range(ns):
+#     name = input("name :")
+#     mk = {}
+#     for j in range(nt):
+#         marks= eval(input(f"enter test {j+1} marks :"))
+#         mk[f'T{j+1}']=marks
+#     result[name]=mk
+    
+# print(result)
+    
+    
+    
+# # multiple department -> name, salary,
+# salary_dp = {}
+# nd = int(input("enter number of dep :"))
+
+# for i in range(nd):
+#     dep = input("enter dep :")
+#     ne= int(input("number of emp :"))
+#     salary_data ={}
+    
+#     for j in range(ne):
+#         name = input("Enter name :")
+#         salary = eval(input("salary :"))
+#         salary_data[name]=salary
+#     salary_dp[dep]=salary_data
+# print(salary_data)
+    
+    
+'''
+while loop ->
+i=value ->initialization
+while cond:
+    # body
+    
+    
+    execute until true inside body
+    stop when false
+'''
+
+
+# num = 1
+# total = 0
+
+# while num <= 5:
+#     total += num
+#     num += 1
+
+# print("Sum is:", total)
+
+
+
+
+
+# num  = 1
+# while num <10:
+#     print("hiiiii")
+#     num=num+1
+# print("code")
+
+
+# present = []
+# next = 'yes'
+# while next == 'yes':
+#     name = input("Enter name : ")
+#     present.append(name)
+#     next = input("do you want to add next student (yes/no) : ")
+# print(present)
+
+
+# numbers = [11,22,33,44,55,66,77,88,99]
+
+# for i in numbers:
+#     if i == 22:
+#         print("value is there:",i)
+
+
+# numbers = [11,22,33,44,55,66,77,88,99]
+# num = eval(input("enter number:"))
+# for i in numbers:
+#     while i == 
+# num = eval()
