2f817d2de82dbec58832a28c64c4bb91e8e4ac82
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-05-30 21:31:53 +0530

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,11 +1,36 @@
+print('python'[0])
+print('hello' *2 + 'world')
+print(5+2.0)
+print('abc'*0)
+print(2**3)
+print(type(None))
+print(5 / 2)
+print(-3 * 3)
 
-# import num
-# num.squre()
+x = 10;
+
+x += 5
+print(x)
+
+print([1,2] is [1,2])
+
+print(5 == 5.0)
 
 
-# from num import divisible
+print(5 is 5)
 
-# divisible()
+print(8 >> 2)
+
+print( 5 ** 0)
+
+print( 5 > 10 or 10 > 5)
+
+x1 = [1,2,3];
+print(x1[0] in x1)
+
+print(3 > 2)
 
 
-# from num import *
+print( 7 % 3)
+
+print(10 >= 10)
