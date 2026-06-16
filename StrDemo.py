3856dc31e212c3a0c91117ea522d3e18c7cec44a
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-06-16 22:58:35 +0530

    String Practice Programm
    String Practice Programm

---
+++ StrDemo.py
@@ -0,0 +1,30 @@
+s = 'Python'
+print(s[35 % len(s)])
+
+
+s = 'java python'
+print(s.startswith('java'))
+
+
+s = 'PY'
+print(s.lower())
+
+
+s = 'Python'
+print(s[25 % len(s)])
+
+s = 'banana'
+print(s.count('a'))
+
+
+s = 'hi'
+print(s.upper())
+
+s = '  hi  '
+print(s.strip())
+
+s = 'java python'
+print(s.startswith('java'))
+
+s = 'Python'
+print(s[15 % len(s)])
