8b459e3612f129438000a5c0eedbb2a0ce81b596
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-09-07 11:42:51 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,48 @@
+for i in range(1, 6):
+    print(i, end=" ")
+    
+    
+    
+for i in range(5):
+    print(i)
+    
+    
+for i in range(5, 0, -1):
+    print(i, end=" ")
+    
+    
+for i in range(1, 10, 2):
+    print(i, end=" ")
+    
+    
+for i in range(2, 11, 2):
+    print(i, end=" ")
+    
+    
+    
+    
+for i in range(1, 4):
+    print(i * i, end=" ")
+    
+    
+total = 0
+
+for i in range(1, 6):
+    total += i
+
+print(total)
+
+
+
+for i in range(3):
+    print("*", end="")
+    
+    
+for i in range(1, 6):
+    if i % 2 == 0:
+        print(i, end=" ")
+        
+        
+for i in range(1, 6):
+    if i % 2 != 0:
+        print(i, end=" ")
