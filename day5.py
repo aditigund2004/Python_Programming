127beb22835c22aea1b17fbb07f8d84a4660e1d7
Aditi Navnath Gund <158809935+aditigund2004@users.noreply.github.com>
2026-09-08 21:50:10 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,48 @@
+for i in range(1, 6):
+    if i == 3:
+        break
+    print(i, end=" ")
+
+
+
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i, end=" ")
+
+
+
+for i in range(2, 5):
+    print(i)
+
+
+
+for i in range(5, 1):
+    print(i)
+
+
+for i in range(3):
+    for j in range(2):
+        print("*", end="")
+
+
+for i in range(3):
+    for j in range(4):
+        print("*")
+
+
+
+for i in range(1, 4):
+    for j in range(1, 3):
+        print(j, end=" ")
+
+
+
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, end=" ")
+
+
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, j)
